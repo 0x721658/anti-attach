d0d97892bd7357a488b5eb7f51914d8a690e7afa
0x8132577 <168567851+0x721658@users.noreply.github.com>
2025-09-01 14:11:14 +1000

    Update attachment.hxx

--- attachment.hxx
+++ attachment.hxx
@@ -1,3 +1,4 @@
+
 #ifndef ATTACH_HXX
 #define ATTACH_HXX
 
@@ -10,98 +11,75 @@
 class already_debugged
 {
 public:
-
-    auto start(int argc, char* argv[]) -> void
-    {
-        this->load();
-
-        if (argc == 1)
-        {
-            this->initialize_main_process();
-        }
-        else
-        {
-            const auto parent_process_id = atoi(argv[1]);
-            this->start_child_process(parent_process_id);
-        }
-    }
+	void dispatch( int argc, char* argv[ ] ) const
+	{
+		argc == 1 ? this->main_process( ) : this->child_process( std::atoi( argv[ 1 ] ) );
+	}
 
 private:
+	void child_process( const unsigned long parent_pid ) const
+	{
+		void* parent_handle = OpenProcess( PROCESS_ALL_ACCESS, 0, parent_pid );
+		if ( parent_handle == nullptr or DebugActiveProcess( parent_pid ) == 0 )
+		{
+			CloseHandle( parent_handle );
+			return;
+		}
 
-    auto load() -> void
-    {
-        static DWORD(WINAPI * NtQuerySystemInformation)(DWORD, PVOID, ULONG, PULONG) = nullptr;
-        static DWORD(WINAPI * NtOpenThread)(HANDLE*, DWORD, OBJECT_ATTRIBUTES*, CLIENT_ID*) = nullptr;
-        static DWORD(WINAPI * NtQueryObject)(HANDLE, DWORD, PVOID, DWORD, DWORD*) = nullptr;
+		void* debug_handle = *( void** ) ( ( unsigned char* ) __readgsqword( 0x30 ) + 0x16a8 );
+		if ( !debug_handle )
+		{
+			CloseHandle( parent_handle );
+			return;
+		}
 
-        const HMODULE ntdll_handle = GetModuleHandleA("ntdll.dll");
-        if (!ntdll_handle)
-            return;
+		void* cloned_handle = nullptr;
+		if ( !DuplicateHandle( GetCurrentProcess( ), debug_handle, parent_handle, &cloned_handle, 0, 0, DUPLICATE_SAME_ACCESS ) )
+		{
+			CloseHandle( parent_handle );
+			return;
+		}
 
-        NtQuerySystemInformation = (DWORD(WINAPI*)(DWORD, PVOID, ULONG, PULONG))GetProcAddress(ntdll_handle, "NtQuerySystemInformation");
-        NtOpenThread = (DWORD(WINAPI*)(HANDLE*, DWORD, OBJECT_ATTRIBUTES*, CLIENT_ID*))GetProcAddress(ntdll_handle, "NtOpenThread");
-        NtQueryObject = (DWORD(WINAPI*)(HANDLE, DWORD, PVOID, DWORD, DWORD*))GetProcAddress(ntdll_handle, "NtQueryObject");
-    }
+		DEBUG_EVENT event;
+		for ( unsigned long timeouts = 0; timeouts < 10;)
+		{
+			if ( !WaitForDebugEvent( &event, 100 ) )
+			{
+				++timeouts;
+				continue;
+			}
 
-    auto start_child_process(const DWORD& parent_process_id) -> void
-    {
-        HANDLE process_handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, parent_process_id);
-        if (!process_handle or DebugActiveProcess(parent_process_id) == 0)
-        {
-            CloseHandle(process_handle);
-            return;
-        }
+			timeouts = 0;
+			ContinueDebugEvent( event.dwProcessId, event.dwThreadId, DBG_CONTINUE );
+		}
 
-        HANDLE debug_handle = *(HANDLE*)((BYTE*)__readgsqword(0x30) + 0x16A8);
-        if (!debug_handle)
-        {
-            CloseHandle(process_handle);
-            return;
-        }
+		CloseHandle( cloned_handle );
+		CloseHandle( debug_handle );
+		CloseHandle( parent_handle );
+		ExitThread( 0 );
+	}
 
-        HANDLE cloned_debug_handle;
-        if (!DuplicateHandle(GetCurrentProcess(), debug_handle, process_handle, &cloned_debug_handle, 0, FALSE, DUPLICATE_SAME_ACCESS))
-        {
-            CloseHandle(process_handle);
-            return;
-        }
+	void main_process( ) const
+	{
+		char process_path[ MAX_PATH ];
+		char command_line[ MAX_PATH + 32 ];
 
-        DEBUG_EVENT debug_event;
-        DWORD timeout_count = 0;
+		{
+			GetModuleFileNameA( nullptr, process_path, sizeof( process_path ) );
+			std::snprintf( command_line, sizeof( command_line ), "\"%s\" %lu", process_path, GetCurrentProcessId( ) );
+		}
 
-        while (timeout_count < 10)
-        {
-            if (WaitForDebugEvent(&debug_event, 100) == 0)
-            {
-                timeout_count++;
-                continue;
-            }
-            timeout_count = 0;
-            ContinueDebugEvent(debug_event.dwProcessId, debug_event.dwThreadId, DBG_CONTINUE);
-        }
+		STARTUPINFOA startup = { sizeof( STARTUPINFOA ) };
+		PROCESS_INFORMATION process = {};
 
-        CloseHandle(cloned_debug_handle);
-        CloseHandle(debug_handle);
-        CloseHandle(process_handle);
-        ExitThread(0);
-    }
-
-    auto initialize_main_process() -> void
-    {
-        STARTUPINFOA startup_info = { sizeof(startup_info) };
-        PROCESS_INFORMATION process_info = {};
-        char current_path[MAX_PATH];
-        GetModuleFileNameA(nullptr, current_path, sizeof(current_path));
-
-        std::string command_line = "\"" + std::string(current_path) + "\" " + std::to_string(GetCurrentProcessId());
-        if (CreateProcessA(nullptr, const_cast<LPSTR>(command_line.c_str()), nullptr, nullptr, FALSE, 0, nullptr, nullptr, &startup_info, &process_info))
-        {
-            CloseHandle(process_info.hProcess);
-            CloseHandle(process_info.hThread);
-            WaitForSingleObject(process_info.hProcess, INFINITE);
-        }
-    }
+		if ( CreateProcessA( nullptr, command_line, nullptr, nullptr, FALSE, 0, nullptr, nullptr, &startup, &process ) )
+		{
+			CloseHandle( process.hProcess );
+			CloseHandle( process.hThread );
+			WaitForSingleObject( process.hProcess, INFINITE );
+		}
+	}
 };
-inline const auto c_already_debugged = std::make_unique<already_debugged>();
+inline const auto c_already_debugged = std::make_unique<already_debugged>( );
 
 #endif // !ATTACH_HXX
